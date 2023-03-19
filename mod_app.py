bf675622d2ed4070b1ccb431baaf70da8cda9c68
MNCEDISIMNCWABE <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-03-19 21:33:09 +0200

    Update mod_app.py

--- mod_app.py
+++ mod_app.py
@@ -29,13 +29,13 @@ end_date = st.date_input('End Date')
 
 # Run Prophet for the selected date range and store the results
 results = run_prophet(model, start_date, end_date)
-# Rename the columns of the results dataframe
-results = results.rename(columns={'ds': 'Date', 'yhat': 'Predicted Price', 'yhat_lower': 'Predicted Lower Bound', 'yhat_upper': 'Predicted Upper Bound'})
-
-
-# Display the predicted closing prices for the selected ticker and date range
-st.write(results)
 
 # Plot the predicted closing prices for the selected ticker and date range
 fig = plot_plotly(model, results)
 st.plotly_chart(fig)
+
+# Rename the columns of the results dataframe
+results = results.rename(columns={'ds': 'Date', 'yhat': 'Predicted Price', 'yhat_lower': 'Predicted Lower Bound', 'yhat_upper': 'Predicted Upper Bound'})
+
+# Display the predicted closing prices for the selected ticker and date range
+st.write(results)
