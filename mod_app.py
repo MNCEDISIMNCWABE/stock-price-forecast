01a7567ab0e8affa8fe568d096c5c5676168feba
MNCEDISIMNCWABE <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-03-19 22:56:50 +0200

    Update mod_app.py

--- mod_app.py
+++ mod_app.py
@@ -5,21 +5,20 @@ from datetime import datetime
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
+from model import get_model, run_prophet
 import plotly
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
-
-from model import get_model, run_prophet
 
 
 # Set the default ticker symbol
 DEFAULT_TICKER = 'WHL.JO'
 
 # Define the Streamlit app
-st.title('Closing Stock Price Forecasting App')
+st.title('Closing Stock Price Predictions')
 
 # Allow the user to input a ticker symbol
-ticker_input = st.text_input('Enter Ticker/Company Symbol', DEFAULT_TICKER)
+ticker_input = st.text_input('Enter Company Symbol', DEFAULT_TICKER)
 
 # Get Prophet model for selected ticker
 model = get_model(ticker_input)
@@ -34,34 +33,11 @@ results = run_prophet(model, start_date, end_date)
 # Plot the predicted closing prices for the selected ticker and date range
 st.write("Prediction Plot:")
 fig = plot_plotly(model, results)
-
-# Create a trace for the actual closing prices
-trace_actual = go.Scatter(
-    x=results['ds'],
-    y=results['y'],
-    mode='lines',
-    name='Actual Closing Price'
-)
-
-# Add the actual closing prices trace to the plot
-fig.add_trace(trace_actual)
-
-# Add a legend to the plot
-fig.update_layout(
-    legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=-0.3,
-        xanchor="center",
-        x=0.5
-    )
-)
-
 st.plotly_chart(fig)
 
 # Rename the columns of the results dataframe
-results = results.rename(columns={'ds': 'Date', 'yhat': 'Predicted Price', 'yhat_lower': 'Predicted Lower Bound', 'yhat_upper': 'Predicted Upper Bound', 'Actual Price': 'Actual Closing Price'})
+results = results.rename(columns={'ds': 'Date', 'yhat': 'Predicted Price', 'yhat_lower': 'Predicted Lower Bound', 'yhat_upper': 'Predicted Upper Bound'})
 
-# Display the predicted and actual closing prices for the selected ticker and date range
-st.write('Predicted and Actual Closing Stock Prices:')
+# Display the predicted closing prices for the selected ticker and date range
+st.write('Predicted Closing Stock Prices:')
 st.write(results)
